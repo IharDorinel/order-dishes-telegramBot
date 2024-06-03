b7550e1b0916ac497d6cee8d79eb08bafe8f359c
Igor Doronin <ihardaronin@gmail.com>
2024-06-03 10:36:02 +0300

    fix some details

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ basket = 0
 
 def start_markup():
     """Creates and returns the initial reply keyboard markup for the bot."""
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=True)
     markup.add('\U0001F4CB Посмотреть меню', f'\U0001F6D2 Корзина ({str(basket)})',
                '\U0001F6F5 Посмотреть статус заказа')
     return markup
@@ -18,20 +18,20 @@ bot = telebot.TeleBot('YOUR_TOKEN_HERE')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     msg = bot.send_message(message.chat.id,
-                           f'Здравствуйте! {message.from_user.first_name}, Я чат-бот,который поможет тебе сделать заказ еды.',
+                           f'Здравствуйте! {message.from_user.first_name}, Я чат-бот, который поможет тебе сделать заказ еды.',
                            reply_markup=start_markup())
 
 
 @bot.message_handler(commands=['feedback'])
 def start_message(message):
     msg = bot.send_message(message.chat.id,
-                           f'Здравствуйте! {message.from_user.first_name}, У нас лучший в городе Ресторан.')
+                           f'Здравствуйте, {message.from_user.first_name}! Оставьте, пожалуйста, отзыв о нашем сервисе.')
 
 
 @bot.message_handler(commands=['support'])
 def start_message(message):
     msg = bot.send_message(message.chat.id,
-                           f'Здравствуйте! {message.from_user.first_name}, Помоги себе сам.')
+                           f'Здравствуйте, {message.from_user.first_name}! Здесь вы можете обратиться за поддержкой.')
 
 
 bot.polling(none_stop=True)
