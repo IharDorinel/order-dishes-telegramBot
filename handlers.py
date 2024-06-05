e7ae36ae38bcbf169dd1c79ff845b6367f496dab
KinzelVA <168530439+KinzelVA@users.noreply.github.com>
2024-06-06 01:55:38 +0700

    add Djstavka

--- handlers.py
+++ handlers.py
@@ -1,15 +1,13 @@
-# Функции-обработчики для команд и нажатия кнопок
-from database import menu
 from telebot import types
+from database import menu  # Предполагается, что у вас есть модуль menu для работы с меню
 
 basket = 0
-
 
 def start_markup():
     """Creates and returns the initial reply keyboard markup for the bot."""
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
     markup.add('\U0001F4CB Посмотреть меню', f'\U0001F6D2 Корзина ({str(basket)})',
-               '\U0001F6F5 Посмотреть статус заказа')
+               '\U0001F6F5 Посмотреть статус заказа', 'Ввести адрес доставки')
     return markup
 
 
@@ -22,13 +20,13 @@ def start_message(message, bot):
 
 
 def feedback_message(message, bot):
-    msg = bot.send_message(message.chat.id,
-                           f'Здравствуйте, {message.from_user.first_name}! Оставьте, пожалуйста, отзыв о нашем сервисе.')
+    bot.send_message(message.chat.id,
+                     f'Здравствуйте, {message.from_user.first_name}! Оставьте, пожалуйста, отзыв о нашем сервисе.')
 
 
 def support_message(message, bot):
-    msg = bot.send_message(message.chat.id,
-                           f'Здравствуйте, {message.from_user.first_name}! Здесь вы можете обратиться за поддержкой.')
+    bot.send_message(message.chat.id,
+                     f'Здравствуйте, {message.from_user.first_name}! Здесь вы можете обратиться за поддержкой.')
 
 
 def category_markup():
@@ -39,7 +37,6 @@ def category_markup():
         markup.add(types.KeyboardButton(f'{emoji} {name}'))
     markup.add(types.KeyboardButton('Назад в основное меню'))  # Добавляем кнопку "Назад в основное меню"
     return markup
-
 
 
 def items_markup(category_name):
@@ -75,14 +72,13 @@ def start_perform_actions(message, bot):
         bot.send_message(message.chat.id, 'Функция корзина')
     elif message.text == '\U0001F6F5 Посмотреть статус заказа':
         bot.send_message(message.chat.id, 'Функция статус заказа')
+    elif message.text == 'Ввести адрес доставки':
+        request_address(message, bot)
 
 
 def category_selected(message, bot):
     if message.text == 'Назад в основное меню':
-        msg = bot.send_message(
-            message.chat.id,
-            reply_markup=start_message(message, bot)
-        )
+        start_message(message, bot)
     else:
         category_name = message.text.split(' ', 1)[1]  # Извлекаем название категории из текста кнопки
         msg = bot.send_message(
@@ -94,7 +90,6 @@ def category_selected(message, bot):
         bot.register_next_step_handler(msg, lambda m: dish_selected(m, bot))
 
 
-
 def dish_selected(message, bot):
     if message.text == 'Назад в категории':
         msg = bot.send_message(
@@ -103,9 +98,7 @@ def dish_selected(message, bot):
             reply_markup=category_markup()
         )
         bot.register_next_step_handler(msg, lambda m: category_selected(m, bot))
-
     else:
-
         dish_name = message.text
         details = menu.dish_details(dish_name)
 
@@ -120,3 +113,24 @@ def dish_selected(message, bot):
                     reply_markup=dish_markup()
                 )
         bot.register_next_step_handler(message, lambda m: dish_selected(m, bot))
+
+
+def address_markup():
+    """Creates and returns the inline keyboard markup for entering the delivery address."""
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add(types.KeyboardButton('Введите адрес доставки'))
+    return markup
+
+
+def request_address(message, bot):
+    """Function to request delivery address from the user."""
+    msg = bot.send_message(message.chat.id, "Пожалуйста, введите свой адрес доставки заказа:", reply_markup=address_markup())
+    bot.register_next_step_handler(msg, lambda m: save_address(m, bot))
+
+
+def save_address(message, bot):
+    """Function to save user's delivery address."""
+    user_id = message.from_user.id
+    address = message.text
+    # В этой функции вы можете сохранить адрес пользователя в базе данных или как-то еще его обработать
+    bot.send_message(message.chat.id, f"Адрес доставки сохранен: {address}")
