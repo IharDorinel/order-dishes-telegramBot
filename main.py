cb14c89c04f5ab30443c27e689cbbb5172bfa340
Alexander <82374540+palex08@users.noreply.github.com>
2024-06-07 01:03:41 +1000

    Add missing function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import feedback as fb
 import handlers
 
 
-bot = telebot.TeleBot('7102874801:AAE4J6_9IcR2kFi2bN-hinUpBEScbyoUSvA')
+bot = telebot.TeleBot('YOUR_TOKEN')
 
 
 commands = [
@@ -37,6 +37,8 @@ def choose_dish_category(call):
 def choose_dish(call):
     fb.fb_dish_selected(call, bot)
 
-
+@bot.callback_query_handler(func=lambda call: call.data.startswith('back:'))
+def back_to_category(call):
+    fb.choose_category(call.message, bot)
 
 bot.polling(none_stop=True)
